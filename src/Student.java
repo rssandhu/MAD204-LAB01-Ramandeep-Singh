c19e2f41466588f7f7236c9e906b5f3b485058de
Ramandeep Singh <raman@insteptechnologies.com>
2025-09-14 22:28:38 -0400

    intial app setup

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+/**
+ * MAD204-01 Lab 1
+ * Author: Ramandeep Singh
+ * Student ID: A00194321
+ * Date: 14/09/2025ramandeepsingh415
+ * Description:
+ *  The Student class represents a student with a name, ID, and five grades.
+ *  It includes methods for setting grades, computing averages, determining letter grades,
+ *  and generating a summary string.
+ */
+
+public class Student {
+
+}
